ed4244954fe4883d0a25f1bc491c55765c01ad59
0m1n0 <min2b12@hotmail.com>
2017-08-25 13:51:30 +0200

    copied from nbs_local.py, result data repository modified, doctoring ongoing

--- reproducibility/reproducibility.py
+++ reproducibility/reproducibility.py
@@ -1,3 +1,182 @@
-from stratipy import load_data
-print(load_data)
-print("Hello from the NBS Docker container !!! Yamaaaaaa _/\_")
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+import os
+# import os.path
+from stratipy import (load_data, formatting_data, filtering_diffusion, clustering, hierarchical_clustering)
+import scipy.sparse as sp
+from scipy.io import loadmat, savemat
+import numpy as np
+import time
+import datetime
+
+from sklearn.grid_search import ParameterGrid
+
+
+"""Tuning parameters' list for Network Based Stratification (NBS)
+
+'param_grid' contains NBS tuning parameters' variable names and its values.
+
+
+Parameters
+----------
+
+data_folder: str
+    Path of data folder including mutation profiles, Protein-Protein Interaction
+    (PPI) networks and result forders.
+
+influence_weight :
+
+simplification : boolean, default: True
+
+compute : boolean, default: False
+    If True, new network influence score will be computed.
+    If False, the latest network influence score  will be taken into
+    account.
+
+overwrite : boolean, default: False
+    If True, new network influence score will be computed even if the file
+    which same parameters already exists in the directory.
+
+alpha : float, default: 0.7
+    Diffusion (propagation) factor with 0 <= alpha <= 1.
+    For alpha = 0 : no diffusion.
+    For alpha = 1 :
+
+tol : float, default: 10e-6
+    Convergence threshold.
+ngh_max : int
+    Number of best influencers in PPI.
+keep_singletons : boolean, default: False
+    If True, proteins not annotated in PPI (genes founded only in patients'
+    mutation profiles) will be also considered.
+    If False, only annotated proteins in PPI will be considered.
+    min_mutation, max_mutation : int
+        Numbers of lowest mutations and highest mutations per patient.
+"""
+
+param_grid = {'data_folder': ['../data/'],
+              'patient_data': ['TCGA_UCEC'],
+              'ppi_data': ['STRING'],
+              'influence_weight': ['min'],
+              'simplification': [True],
+              'compute': [True],
+              'overwrite': [False],
+              'alpha': [0.7],
+              'tol': [10e-3],
+              'ngh_max': [11],
+              'keep_singletons': [False],
+              'min_mutation': [10],
+              'max_mutation': [200000],
+              'qn': ['mean'],
+              'n_components': [3],
+              'n_permutations': [100, 1000],
+              'run_bootstrap': [True],
+              'run_consensus': [True],
+              'lambd': [1, 1800],
+              'tol_nmf': [1e-3],
+              'linkage_method': ['average']
+              }
+
+# NOTE sys.stdout.flush()
+
+@profile
+def all_functions(params):
+
+    if alpha == 0 and qn is not None:
+        print('############ PASS ############')
+        pass
+
+    else:
+        result_folder = 'reproducibility_data/' + 'result_' + patient_data + '_' + ppi_data + '/'
+        print(result_folder)
+        print("alpha =", alpha)
+        print("QN =", qn)
+        print("k =", n_components)
+        print("lambda =", lambd)
+        print("PPI network =", ppi_data)
+
+        # ------------ load_data.py ------------
+        print("------------ load_data.py ------------")
+        (patient_id, mutation_profile, gene_id_patient,
+         gene_symbol_profile) = load_data.load_TCGA_UCEC_patient_data(
+             data_folder)
+
+        gene_id_ppi, network = load_data.load_PPI_String(data_folder, ppi_data)
+
+        # ------------ formatting_data.py ------------
+        print("------------ formatting_data.py ------------")
+        (network, mutation_profile,
+         idx_ppi, idx_mut, idx_ppi_only, idx_mut_only) = (
+            formatting_data.classify_gene_index(
+                network, mutation_profile, gene_id_ppi, gene_id_patient))
+
+        (ppi_total, mut_total, ppi_filt, mut_filt) = (
+            formatting_data.all_genes_in_submatrices(
+                network, idx_ppi, idx_mut, idx_ppi_only, idx_mut_only,
+                mutation_profile))
+
+        # ------------ filtering_diffusion.py ------------
+        print("------------ filtering_diffusion.py ------------")
+        final_influence = (
+            filtering_diffusion.calcul_final_influence(
+                sp.eye(ppi_filt.shape[0], dtype=np.float32), ppi_filt,
+                result_folder, influence_weight, simplification,
+                compute, overwrite, alpha, tol))
+
+        ppi_final, mut_final = filtering_diffusion.filter_ppi_patients(
+            ppi_total, mut_total, ppi_filt, final_influence, ngh_max,
+            keep_singletons, min_mutation, max_mutation)
+
+        mut_type, mut_propag = filtering_diffusion.propagation_profile(
+            mut_final, ppi_filt, alpha, tol, qn)
+
+        # ------------ clustering.py ------------
+        print("------------ clustering.py ------------")
+        genes_clustering, patients_clustering = (clustering.bootstrap(
+            result_folder, mut_type, mut_propag, ppi_final,
+            influence_weight, simplification,
+            alpha, tol, keep_singletons, ngh_max, min_mutation, max_mutation,
+            n_components, n_permutations,
+            run_bootstrap, lambd, tol_nmf))
+
+        distance_genes, distance_patients = clustering.consensus_clustering(
+            result_folder, genes_clustering, patients_clustering,
+            influence_weight, simplification, mut_type,
+            alpha, tol, keep_singletons, ngh_max, min_mutation, max_mutation,
+            n_components, n_permutations, run_consensus, lambd, tol_nmf)
+
+        # ------------ hierarchical_clustering.py ------------
+        print("------------ hierarchical_clustering.py ------------")
+        # hierarchical_clustering.distance_matrix(
+        #     hierarchical_factorization_directory, distance_patients, ppi_data,
+        #     mut_type,
+        #     alpha, tol,  keep_singletons, ngh_max, min_mutation, max_mutation,
+        #     n_components, n_permutations, lambd, tol_nmf, linkage_method)
+        hierarchical_clustering.distance_patients_from_consensus_file(
+            result_folder, distance_patients, ppi_data, mut_type,
+            influence_weight, simplification, alpha, tol,  keep_singletons,
+            ngh_max, min_mutation, max_mutation, n_components, n_permutations,
+            lambd, tol_nmf, linkage_method)
+
+
+if (sys.version_info < (3, 2)):
+    raise "Must be using Python ≥ 3.2"
+
+else:
+    start_all = time.time()
+    for params in list(ParameterGrid(param_grid)):
+        start = time.time()
+        print(params)
+        for i in params.keys():
+            exec("%s = %s" % (i, 'params[i]'))
+        all_functions(params)
+        end = time.time()
+        print('---------- ONE STEP = {} ---------- {}'
+              .format(datetime.timedelta(seconds=end-start),
+                      datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+
+    end_all = time.time()
+    print('---------- ALL = {} ---------- {}'
+          .format(datetime.timedelta(seconds = end_all - start_all),
+                  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
