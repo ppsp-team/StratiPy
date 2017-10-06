b57254cf3ce5d70931a6a1f7929189c8b948c794
0m1n0 <min2b12@hotmail.com>
2017-10-06 11:17:58 +0200

    clean debris

--- reproducibility/confusion_matrices.py
+++ reproducibility/confusion_matrices.py
@@ -45,45 +45,6 @@ def get_cluster_idx(result_folder_repro, method, n_permutations, replace=False,
 
     return cluster_idx
 
-#
-# def loading_hierarchical_clustering_data_lamb1_1800(result_folder_repro):
-#     print(" ==== loading Hierarchical clustering data ")
-#     # # Hofree's result --- 100 permutations of bootstrap
-#     # hof_100_data = loadmat(result_folder_repro + 'results_NBS_Hofree_100.mat')
-#     #
-#     # # StratiPy's result --- 100 permutations of bootstrap
-#     # # result with lambda = 1 and 1800
-#     # stp_100_data_lamb1 = loadmat(result_folder_repro +
-#     #                              'result_TCGA_UCEC_STRING/hierarchical_clustering/mean_qn/gnmf/hierarchical_clustering_Patients_weight=min_simp=True_alpha=0.7_tol=0.01_singletons=False_ngh=11_minMut=10_maxMut=200000_comp=3_permut=100_lambd=1_tolNMF=0.001_method=average.mat')
-#     # stp_100_data_lamb1800 = loadmat(result_folder_repro +
-#     #                                 'result_TCGA_UCEC_STRING/hierarchical_clustering/mean_qn/gnmf/hierarchical_clustering_Patients_weight=min_simp=True_alpha=0.7_tol=0.01_singletons=False_ngh=11_minMut=10_maxMut=200000_comp=3_permut=100_lambd=1800_tolNMF=0.001_method=average.mat')
-#     #
-#     # # Get number of subgroups for each patient
-#     # hof_100_3cluster = hof_100_data['NBS_cc_label'].squeeze().tolist()
-#     # stp_100_lamb1_3cluster = list(stp_100_data_lamb1['flat_cluster_number'][0])
-#     # stp_100_lamb1800_3cluster = list(stp_100_data_lamb1800['flat_cluster_number'][0])
-#     #
-#     # # Cooridnate Stratipy's cluster index with Hofree's cluster index
-#     # # clust(Hofree) 2(1) <-> clust(Stratipy) 1(2)
-#     # stp_100_lamb1_3cluster = replace_list_element(stp_100_lamb1_3cluster, 2, 0) # 2 -> 0
-#     # stp_100_lamb1_3cluster = replace_list_element(stp_100_lamb1_3cluster, 1, 2) # 1 -> 2
-#     # stp_100_lamb1_3cluster = replace_list_element(stp_100_lamb1_3cluster, 0, 1) # 0 -> 2
-#     #
-#     # stp_100_lamb1800_3cluster = replace_list_element(stp_100_lamb1800_3cluster, 2, 0) # 2 -> 0
-#     # stp_100_lamb1800_3cluster = replace_list_element(stp_100_lamb1800_3cluster, 1, 2) # 1 -> 2
-#     # stp_100_lamb1800_3cluster = replace_list_element(stp_100_lamb1800_3cluster, 0, 1) # 0 -> 2
-#
-#     print(" ==== creating Confusion matrices ")
-#     # confusion matrice between Hofree & StratiPy (lambda = 1)
-#     conf_lamb1 = confusion_matrix(hof_100_3cluster, stp_100_lamb1_3cluster)
-#     conf_lamb1 = np.around((conf_lamb1.astype('float') / conf_lamb1.sum(axis=1)[:, np.newaxis]), decimals=2)
-#
-#     # confusion matrice between Hofree & StratiPy (lambda = 1800)
-#     conf_lamb1800 = confusion_matrix(hof_100_3cluster, stp_100_lamb1800_3cluster)
-#     conf_lamb1800 = np.around((conf_lamb1800.astype('float') / conf_lamb1800.sum(axis=1)[:, np.newaxis]), decimals=2)
-#
-#     return conf_lamb1, conf_lamb1800
-
 
 def plot_confusion_matrix(result_folder_repro, M, plot_title, param_value):
     norm_conf = []
@@ -125,18 +86,6 @@ def plot_confusion_matrix(result_folder_repro, M, plot_title, param_value):
     plot_name = 'confusion_matrix_' + param_value.replace(" ", "")
     plt.savefig('{}{}.pdf'.format(result_folder_repro, plot_name),
                 bbox_inches='tight')
-
-#
-# def reproducibility_confusion_matrices(result_folder_repro, param_val1, param_val2):
-#     conf_lamb1, conf_lamb1800 = loading_hierarchical_clustering_data_lamb1_1800(
-#         result_folder_repro)
-#     print(" ==== plotting and saving Confusion matrices ")
-#     plot_confusion_matrix(result_folder_repro, conf_lamb1,
-#                            'Confusion matrix\nwith reported tuning parameter value',
-#                            param_val1)
-#     plot_confusion_matrix(result_folder_repro, conf_lamb1800,
-#                            'Confusion matrix\nwith actually used tuning parameter value',
-#                            param_val2)
 
 
 def repro_confusion_matrix(result_folder_repro, data1, data2, plot_title,
