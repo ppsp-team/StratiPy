bc76fd498391895e2cfabda5052333c4ef4e06d4
0m1n0 <min2b12@hotmail.com>
2018-04-27 19:16:32 +0200

    SSC result analysis added: statistical test and barplot

--- stratipy/hierarchical_clustering.py
+++ stratipy/hierarchical_clustering.py
@@ -119,3 +119,113 @@ def distance_patients_from_consensus_file(
         # print("---------- Save time = {} ---------- {}"
         #       .format(datetime.timedelta(seconds=end-start),
         #               datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+
+
+def get_individual_lists_from_clusters(data_folder, ssc_type, ssc_subgroups, result_folder,
+                                      influence_weight, simplification, alpha, tol, keep_singletons,
+                                       ngh_max, min_mutation, max_mutation, n_components,
+                                       n_permutations, lambd, tol_nmf, linkage_method):
+    mutation_profile_file = (data_folder + "{}_{}_mutation_profile.mat"
+                             .format(ssc_subgroups, ssc_type))
+    loadfile = loadmat(mutation_profile_file)
+    indiv = (loadfile['indiv'].flatten()).tolist()
+
+    hierarchical_clustering_file = (
+    result_folder+'hierarchical_clustering/' + mut_type + '/' + 'nmf/' +
+    'hierarchical_clustering_Patients_weight={}_simp={}_alpha={}_tol={}_singletons={}_ngh={}_minMut={}_maxMut={}_comp={}_permut={}_lambd={}_tolNMF={}_method={}.mat'
+    .format(influence_weight, simplification, alpha, tol, keep_singletons,
+            ngh_max, min_mutation, max_mutation, n_components,
+            n_permutations, lambd, tol_nmf, linkage_method))
+
+    h = loadmat(hierarchical_clustering_file)
+    clust_nb = np.squeeze(h['flat_cluster_number']) # cluster index for each individual
+    idx = np.squeeze(h['dendrogram_index']) # individuals' index
+
+    # load individual data including sex and IQ
+    df_ssc = pd.read_csv(data_folder + '{}_indiv_sex_iq.csv'
+                                 .format(ssc_subgroups), sep='\t')
+    ind_ssc_raw = df_ssc['individual'].tolist()
+    df_ssc_iq = df_ssc[df_ssc['iq'].notnull()]
+    ind_ssc_iq = df_ssc_iq['individual'].tolist()
+
+    # load distance_CEU data
+    df_dist = pd.read_csv(data_folder + 'SSC_distanceCEU.csv', sep="\t")
+    ind_dist = df_dist.individual.tolist()
+
+    clusters = list(set(clust_nb))
+    # total_cluster_list = []
+    siblings_cluster_list = []
+    probands_cluster_list = []
+    female_cluster_list = []
+    male_cluster_list = []
+    iq_cluster_list = []
+    distCEU_list = []
+
+    for i, cluster in enumerate(clusters):
+        idCluster = [i for i,c in enumerate(clust_nb) if c==cluster]
+        subjs = [indiv[i] for i in idx[idCluster]]
+    #     total_cluster_list.append(len(subjs))
+
+        sib_indiv = [i for i in subjs if i[-2:-1]=='s'] # individuals' ID list
+        siblings_cluster_list.append(len(sib_indiv))
+        prob_indiv = [i for i in subjs if i[-2:-1]=='p'] # individuals' ID list
+        probands_cluster_list.append(len(prob_indiv))
+
+        # sex count in each cluster
+        sex_list = [df_ssc['sex'].iloc[ind_ssc_raw.index(i)] for i in subjs if i in ind_ssc_raw]
+        female_cluster_list.append(sex_list.count('female'))
+        male_cluster_list.append(sex_list.count('male'))
+
+        # get IQ list for each cluster
+        iq_list = [df_ssc_iq['iq'].iloc[ind_ssc_iq.index(i)] for i in subjs if i in ind_ssc_iq]
+        iq_list = [int(i) for i in a] # element type: np.float -> int
+        # create frequency (count individuals for each IQ) list
+        iq_count_list = [0]*int(max(iq_list)+1)
+        for i in iq_list:
+            iq_count_list[i] = iq_list.count(i)
+        iq_cluster_list.append(iq_count_list)
+
+        # get distance CEU for each cluster
+        distCEU_list.append([df_dist['distanceCEU'].iloc[ind_dist.index(i)] for i in subjs if i in ind_dist])
+
+    # Fisher's exact test between probands/siblings
+    prob_sib = [probands_cluster_list, siblings_cluster_list]
+    print("Probands / Siblings (Fisher's exact):", fisher_exact(prob_sib))
+
+    # sex
+    male_female = [male_cluster_list, female_cluster_list]
+    print("Males / Females (Fisher's exact):", fisher_exact(male_female))
+
+    # concatenate IQ lists and remove zero columns (chi square test could not apply)
+    iq_array = np.array(iq_cluster_list)
+    iq_array = np.delete(iq_array, (iq_array==0).any(0), axis=1)
+    print("IQ (Chi 2): ", chisquare(iq_array[0],iq_array[1]))
+
+    # Distance_CEU distribution between 2 samples
+    print("Distance_CEU (Kolmogorov-Smirnov):", ks_2samp(distCEU_list[0], distCEU_list[1]))
+
+    return siblings_cluster_list, probands_cluster_list, female_cluster_list, male_cluster_list
+
+
+def stacked_bar_plot(n_components, siblings_cluster_list, probands_cluster_list, odd, p_val,
+                     female_cluster_list, male_cluster_list):
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    X = range(n_components)
+
+    ax.bar(X, probands_cluster_list, label='Probands', align='edge', width=-0.3, edgecolor='w', color='gray',
+           bottom=siblings_cluster_list)
+    ax.bar(X, siblings_cluster_list, label='Siblings', align='edge', width=-0.3, edgecolor='w', color='lightgray')
+
+    ax.bar(X, male_cluster_list, label='Male', align='edge', width=0.3, edgecolor='w', color='steelblue',
+           bottom=female_cluster_list)
+    ax.bar(X, female_cluster_list, label='Female', align='edge', width=0.3, edgecolor='w', color='lightcoral')
+
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+#     ax.set_title("{} - {}: {}\n---------------------------\nFisher's exact test\nodds ratio = {:0.2e}\np-value = {:0.2e}"
+#                  .format(ssc_type, ssc_subgroups, mut_type, round(odd), (p_val)))
+    ax.set_title("{} - {}: {}\n".format(ssc_type, ssc_subgroups, mut_type))
+    plt.xticks(X, list(range(1,n_components+1)))
+    plt.show()
